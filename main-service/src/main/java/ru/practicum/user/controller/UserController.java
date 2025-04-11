2659864889494c0f260f79399009d81965c15d77
HaerDalisBlade <HaerDalisBlade@yandex.ru>
2025-04-11 18:09:12 +0300

    style corrections

--- main-service/src/main/java/ru/practicum/user/controller/UserController.java
+++ main-service/src/main/java/ru/practicum/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package ru.practicum.user.controller;
 
 import jakarta.validation.Valid;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
